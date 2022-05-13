6c650cbf374312bfd489451c9610706559282153
Jon Lervold <jonlervold@gmail.com>
2022-05-13 05:26:01 -0600

    Initial commit

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,14 +1,37 @@
+import axios from 'axios';
+import { useState } from 'react';
 import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
 
 const StyledApp = styled.div`
   // Your style here
 `;
 
 export function App() {
+  const [generatedLyrics, setGeneratedLyrics] = useState<
+    string | JSX.Element[]
+  >('Push Generate');
+
+  const handleGenerate = async () => {
+    const res: { data: string } = await axios.get(
+      'https://azrpmnicq4kbbzcvbxbkkfozra0swfty.lambda-url.us-east-1.on.aws'
+    );
+    console.log(res.data);
+    const newLyrics = res.data.split('\n').map((item) => {
+      return (
+        <span>
+          {item}
+          <br />
+        </span>
+      );
+    });
+
+    setGeneratedLyrics(newLyrics);
+  };
+
   return (
     <StyledApp>
-      <NxWelcome title="app" />
+      <button onClick={handleGenerate}>Generate!</button>
+      <p>{generatedLyrics}</p>
     </StyledApp>
   );
 }
