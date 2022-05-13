a0c26e328960cf4dcf32ae17128c8d34a6ffdf27
Jon Lervold <jonlervold@gmail.com>
2022-05-13 15:54:39 -0600

    Operational, ugly, need to pause

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,15 +1,36 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import './App.css';
+import Lyrics from './components/Lyrics';
 
 const StyledApp = styled.div`
-  // Your style here
+  font-family: Monaco, monospace;
+  color: white;
+  text-align: center;
+  justify-content: center;
+  img {
+    align-self: center;
+  }
+`;
+const MainBlock = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+const Card = styled.div`
+  width: 40%;
+  height: 636px;
+  background-color: gray;
+  display: inline-block;
+  margin: 0.5rem;
+  overflow: scroll;
 `;
 
 export function App() {
   const [generatedLyrics, setGeneratedLyrics] = useState<
     string | JSX.Element[]
-  >('Push Generate');
+  >('Wait');
 
   const handleGenerate = async () => {
     const res: { data: string } = await axios.get(
@@ -30,8 +51,16 @@ export function App() {
 
   return (
     <StyledApp>
+      <img src="../assets/dashes.png" />
+      <img src="../assets/app-title.png" />
+      <img src="../assets/dashes.png" />
+      <MainBlock>
+        <img src="../assets/cedric.png" />
+        <Card>{generatedLyrics}</Card>
+        {/* {generatedLyrics} */}
+      </MainBlock>
       <button onClick={handleGenerate}>Generate!</button>
-      <p>{generatedLyrics}</p>
+      <button>About Cedric Bixler Lyrics Generator</button>
     </StyledApp>
   );
 }
